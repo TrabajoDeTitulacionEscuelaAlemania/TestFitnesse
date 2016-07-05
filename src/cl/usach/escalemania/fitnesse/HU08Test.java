c061517c02ace4d4c6423889a5f3eca64c9b3130
gigoox <rodrigo.rivas@usach.cl>
2016-07-05 13:55:02 -0300

    Probada HU-08

--- src/cl/usach/escalemania/fitnesse/HU08Test.java
+++ src/cl/usach/escalemania/fitnesse/HU08Test.java
@@ -5,6 +5,8 @@
  */
 package cl.usach.escalemania.fitnesse;
 
+import cl.usach.escalemania.sessionbeans.SimulacionFacadeLocal;
+import fit.ColumnFixture;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -12,13 +14,28 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 
-public class HU08Test {
+public class HU08Test extends ColumnFixture{
     
+    String nombrePrograma;
     
      public InitialContext ctx() throws IOException, NamingException{
         Properties props = new Properties();
         props.load(new FileInputStream("C:\\Users\\Rodrigo Rivas\\Documents\\NetBeansProjects\\TestFitnesse\\nbproject\\jndi.properties"));
         InitialContext ctx = new InitialContext(props);
         return ctx;
+    }
+     
+    public double simulacionNota() throws IOException, NamingException{
+        SimulacionFacadeLocal simulacionFacade=(SimulacionFacadeLocal)ctx().lookup
+        ("cl.usach.escalemania.sessionbeans.SimulacionFacadeLocal");
+        double resultado=simulacionFacade.notaSimulacion(nombrePrograma);
+        return resultado;
+    }
+    
+    public double simulacionPorcentaje() throws IOException, NamingException{
+        SimulacionFacadeLocal simulacionFacade=(SimulacionFacadeLocal)ctx().lookup
+        ("cl.usach.escalemania.sessionbeans.SimulacionFacadeLocal");
+        double resultado=simulacionFacade.porcentajeSimulacion(nombrePrograma);
+        return resultado;
     }
 }
